66ebccdd0c5241815a1946ba61074405741eda67
unknown <abdirisaq.omer@nology.io>
2020-04-09 11:58:01 +0100

    Done

--- main.js
+++ main.js
@@ -1,79 +1,63 @@
-const char = {  
-    0: "-----",
-    1: ".----",
-    2: "..---",
-    3: "...--",
-    4: "....-",
-    5: ".....",
-    6: "-....",
-    7: "--...",
-    8: "---..",
-    9: "----.",
-    a: ".-",
-    b: "-...",
-    c: "-.-.",
-    d: "-..",
-    e: ".",
-    f: "..-.",
-    g: "--.",
-    h: "....",
-    i: "..",
-    j: ".---",
-    k: "-.-",
-    l: ".-..", 
-    m: "--",
-    n: "-.",
-    o: "---",
-    p: ".--.",
-    q: "--.-",
-    r: ".-.",
-    s: "...",
-    t: "-",
-    u: "..-",
-    v: "...-",
-    w: ".--",
-    x: "-..-",
-    y: "-.--",
-    z: "--..",
-    " ": " "
+const char = {
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  " ": " ",
 };
 
-// First Function is to translate it from text to Morse
-const fromLettersToMorse = textInput => {
-    textInput.toLowerCase().split("").map(mapLetter).join(" ");
-}
+const translateToMorse = (morse) =>
+  morse.toLowerCase().split("").map(mapLetter).join(" ");
+const translateToText = (morse) =>
+  morse
+    .split("   ")
+    .map((word) => word.split(" ").map(changeToWords).join(""))
+    .join(" ");
 
-//This is from morse to letters
-const fromMorseToLetters = morseInput => {
-    morseInput.split(" ").map(changeToWordsReverse).join("");
-}
+const mapLetter = (letter) => char[letter];
 
-
-const mapLetter = letter => char[letter];
-
-//This is a fuction when inputed by the user it returs the value key from the array e.g "u" will return "..-"
-
-const changeToWordsReverse = inputFromUser => {
-    return Object.keys(char).find(key => char[key] === inputFromUser);
-}
+const changeToWords = (element) => {
+  return Object.keys(char).find((key) => char[key] === element);
+};
 
 const writeOutput = () => {
-    let message = document.getElementById("text").value;
-    if (message[0].includes(".") || message[0].includes("-")) {
-        return document.getElementById("result").value = fromMorseToLetters(message);
-    } document.getElementById("result").value = fromLettersToMorse(message);
-}
+  let message = document.getElementById("text").value;
+  if (message[0].includes(".") || message[0].includes("-")) {
+    return (document.getElementById("result").value = translateToText(message));
+  }
+  document.getElementById("result").value = translateToMorse(message);
+};
 
 document.getElementById("translate").addEventListener("click", writeOutput);
-
-
-
-
-
-
-
-
-
-
-
-
